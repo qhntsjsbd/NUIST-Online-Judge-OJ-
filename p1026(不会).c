c970fcb88f1ad3c10fe56f79d7a9e206ce21bb75
Viayou <78718947+qhntsjsbd@users.noreply.github.com>
2022-12-13 10:06:46 +0800

    Update p1026(不会).c

--- p1026(不会).c
+++ p1026(不会).c
@@ -1,54 +1,21 @@
-//#include<stdio.h>
-//#include<math.h>
-//long long int jiec(int n);
-//int main()
-//{
-//	long long int n, i,t, j = 0, y = 0;
-//	
-//	     while(scanf("%d", &n)!=EOF)
-//		 {t = jiec(n);
-//		if (n <4)
-//			printf("%d", 0);
-//		else
-//		{   j=0;
-//			for (i = 1;; i++)
-//			{
-//				y = t % (int)pow(10, i);
-//				if (y == 0)
-//					j++;
-//				if (y != 0)
-//					break;
-//			}
-//			printf("%d\n", j);
-//		}}
-//		
-//		
-//	}
-//
-//
-//long long int jiec(int n)
-//{
-//	if (n == 1||n==0)
-//		return 1;
-//	else
-//		return n * jiec(n - 1);
-//}
-//#include <stdio.h>
-//int main() {
-//    int a, ans = 0;
-//    while (scanf("%d", &a) != EOF)
-//    {int count = 0;
-// for (; ;) {
-// a = a/5;
-// if (a == 0) {
-// break;
-// }
-// else {
-// count+= a;
-// }
-// }
-// printf("%d\n",count);
-//    }
-//    
-//    return 0;
-//}
+//Viayou专属
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+    int n;
+    while(scanf("%d",&n)!=EOF)
+    {
+	
+    int count = 0;
+    while(n>0){
+        count = count + n / 5;
+        n = n / 5;
+    }
+    printf("%d\n",count);
+}
+    return 0;
+}
+//没事翻翻github没想到我校2022新更了一个，来水一发
+//ac代码
